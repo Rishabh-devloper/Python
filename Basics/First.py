863f3c4626f9f3be63377a2e40d575a4055fe104
Rishabh-devloper <rishabhmishra143a@gmail.com>
2025-10-19 11:53:59 +0530

    first commit

---
+++ Basics/First.py
@@ -0,0 +1,13 @@
+# st='sherr'
+# newst=''
+# for char in range (4,-1,-1):
+#   newst+=st[char]
+# print(newst)
+
+# name= 'Rishabh Mishra'
+# print(name[:7:1])
+
+# a = "12"
+
+# b=int(a)
+# print(b)
