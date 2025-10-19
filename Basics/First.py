35c2ff21db8df7469957ac87a0cccacead5fb497
Rishabh-devloper <rishabhmishra143a@gmail.com>
2025-10-19 12:29:28 +0530

    if else question of python

--- Basics/First.py
+++ Basics/First.py
@@ -1,13 +1,61 @@
 # st='sherr'
-# newst=''
-# for char in range (4,-1,-1):
-#   newst+=st[char]
-# print(newst)
+"""Q1. Accept two numbers and print the greatest between them.
+Q2. Accept the gender from the user as char and print the
+respective greeting message
+Ex - Good Morning Sir (on the basis of gender)
+Q3. Accept an integer and check whether it is an even number
+or odd.
+Q4. Accept name and age from the user. Check if the user is a
+valid voter or not.
+Ex- “hello shery you are a valid voter”
+Q5. Accept a year and check if it a leap year or not (google to
+find out what is a leap year)"""
 
-# name= 'Rishabh Mishra'
-# print(name[:7:1])
+# Q1.
+# num1=int(input("enter first number"))
+# num2 = int(input("enter 2 number"))
 
-# a = "12"
+# if num1<num2:
+#     print(f"{num2} is greater than {num1}")
+# elif num1==num2:
+#     print(f"{num2} is equal to  {num1}")
+# else:
+#     print(f"{num1} is greater than {num2}")
 
-# b=int(a)
-# print(b)
+# #Q2
+# def Greeting(gen):
+#     if gen=="male" :
+#         print("good morning sir ")
+#     elif gen=="female":
+#         print("good morning Maam")
+#     else:
+#         print("enter correct gender")
+
+
+# gen=input("enter the gender")
+# Greeting(gen)
+
+# Q4
+# def valid (name , age):
+#     if age>18 :
+#         print(f"Hey {name} You are valid for vote")
+#     else :
+#         print(F"{name} you are valid Voter ")
+
+# name = input("enter Your Name")
+# age=int(input("enter your age"))
+# valid(name , age)
+
+# Q5 
+# def leap_year (year):
+#     if year % 400 == 0 :
+#         print(f"{year} is leap year")
+    
+#     elif year %100 == 0:
+#         print(f"{year} is not leap year")
+#     elif year% 4 == 0 :
+#         print(f"{year} is leap year")
+#     else:
+#         print(f"{year} is not leap year")
+# year = int(input("enter year"))
+# leap_year(year)
